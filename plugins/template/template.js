81dc917ed497f580282bc937245cb8b979bcc2c9
mde <mde@c3ab02eb-571f-0410-ad69-ce51ba5a7568>
2007-07-02 05:58:52 +0000

    Added basic JS templating plugin.
    git-svn-id: http://svn.fleegix.org/fleegix_js/trunk@110 c3ab02eb-571f-0410-ad69-ce51ba5a7568

---
+++ plugins/template/template.js
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2006 Matthew Eernisse (mde@fleegix.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+if (typeof fleegix == 'undefined') { throw('Requires fleegix base namespace.'); }
+if (typeof fleegix.xhr == 'undefined') { throw('Requires fleegix.xhr namespace.'); }
+if (typeof fleegix.template == 'undefined') { fleegix.template = {}; }
+// single params Object can have the following properties:
+// markup -- String, a string of markup with vars to interpolate
+//    if this exists, markupFile will be ignored
+// markupFile -- String, a path to a file containing string data
+//    then used to set the markup prop
+// data -- Object, keyword/vals to use for the variable substitution
+// requireAllKeys -- Boolean, if set to true, any variables in the
+//    template file that don't have corresponding keys in the
+//    data object will still show in the template as
+//    ${ variable_name }. If set to false, those variables in
+//    the template are replaced with empty strings
+fleegix.template.Templater = function (p) {
+  var params = p || {}
+  var opts = params.options || {};
+  this.markup = params.markup || '';
+  this.markupFile = '';
+  this.data = params.data || {};
+  this.requireAllKeys = typeof opts.requireAllKeys == 'boolean' ?
+    opts.requireAllKeys : true;
+  if (!this.markup) {
+    var _this = this;
+    this.markupFile = params.markupFile;
+    var s = fleegix.template.markupCache[this.markupFile];
+    if (!s || this.preventCache) {
+      var p = {
+        url: this.markupFile,
+        method: 'GET',
+        preventCache: true,
+        async: false
+      };
+      s = fleegix.xhr.doReq(p);
+      fleegix.template.markupCache[this.markupFile] = s;
+    }
+    this.markup = s;
+  }
+}
+fleegix.template.Templater.prototype.templatize =
+  function (domNode) {
+  var str = this.getTemplatedMarkup();
+  domNode.innerHTML = str;
+  return domNode;
+}
+fleegix.template.Templater.prototype.getTemplatedMarkup =
+  function () {
+  var pat = /\$\{ *(.*?) *}/g;
+  var subPat = /[${} ]/g
+  var str = this.markup;
+  var match = str.match(pat);
+  for (var i = 0; i < match.length; i++) {
+    m = match[i];
+    key = m.replace(subPat, '');
+    var data = this.data[key] ? this.data[key] : '';
+    if (data || !this.requireAllKeys) {
+      str = str.replace(m, data);
+    }
+  }
+  return str;
+};
+fleegix.template.markupCache = {};
