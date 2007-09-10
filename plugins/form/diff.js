a893f1d0b6724f0dcfdf475e12afdb4438699e9d
mde <mde@c3ab02eb-571f-0410-ad69-ce51ba5a7568>
2007-09-10 07:28:27 +0000

    Adding the form.diff and form.restore plugin files.
    git-svn-id: http://svn.fleegix.org/fleegix_js/trunk@140 c3ab02eb-571f-0410-ad69-ce51ba5a7568

---
+++ plugins/form/diff.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2007 Matthew Eernisse (mde@fleegix.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+if (typeof fleegix == 'undefined') { var fleegix = {}; }
+if (typeof fleegix.form == 'undefined') { var fleegix.form = {}; }
+if (typeof fleegix.form.valuesToJSObject == 'undefined') {
+  throw('fleegix.form.diff depends on fleegix.form.valuesToJSObject.'); }
+
+fleegix.form.diff = function (formUpdated, formOrig, opts) {
+  var o = opts || {};
+  // Accept either form or hash-conversion of form
+  var hUpdated = formUpdated.toString() == '[object HTMLFormElement]' ?
+    fleegix.form.valuesToJSObject(formUpdated) : formUpdated;
+  var hOrig = formOrig.toString() == '[object HTMLFormElement]' ?
+    fleegix.form.valuesToJSObject(formOrig) : formOrig;
+  var diffs = [];
+  var count = 0;
+
+  function addDiff(n, hA, hB, secondPass) {
+    if (!diffs[n]) {
+      count++;
+      diffs[n] = secondPass? [hB[n], hA[n]] :
+        [hA[n], hB[n]];
+    }
+  }
+
+  function diffSweep(hA, hB, secondPass) {
+    for (n in hA) {
+      // Elem doesn't exist in B
+      if (typeof hB[n] == 'undefined') {
+        // If intersectionOnly flag set, ignore stuff that's
+        // not in both sets
+        if (o.intersectionOnly) { continue; };
+        // Otherwise we want the union, note the diff
+        addDiff(n, hA, hB, secondPass);
+      }
+      // Elem exists in both
+      else {
+        v = hA[n];
+        // Multi-value -- array, hA[n] actually has values
+        if (v instanceof Array) {
+          if (!hB[n] || (hB[n].toString() != v.toString())) {
+            addDiff(n, hA, hB, secondPass);
+          }
+        }
+        // Single value -- null or string
+        else {
+          if (hB[n] != v) {
+            addDiff(n, hA, hB, secondPass);
+          }
+        }
+      }
+    }
+  }
+  // First sweep check all items in updated
+  diffSweep(hUpdated, hOrig, false);
+  // Second sweep, check all items in orig
+  diffSweep(hOrig, hUpdated, true);
+
+  // Return an obj with the count and the hash of diffs
+  return {
+    count: count,
+    diffs: diffs
+  };
+};
+
+
---
+++ plugins/form/restore.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2007 Matthew Eernisse (mde@fleegix.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+if (typeof fleegix == 'undefined') { var fleegix = {}; }
+if (typeof fleegix.form == 'undefined') { var fleegix.form = {}; }
+
+fleegix.form.restore = function (form, str, o) {
+  var opts = o || {};
+  var arr = str.split('&');
+  var d = {};
+  for (var i = 0; i < arr.length; i++) {
+    var pair = arr[i].split('=');
+    var name = pair[0];
+    var val = pair[1];
+    if (typeof d[name] == 'undefined') {
+      d[name] = val;
+    }
+    else {
+      if (!(d[name] instanceof Array)) {
+        var t = d[name];
+        d[name] = [];
+        d[name].push(t);
+      }
+      d[name].push(val);
+    }
+  }
+  for (var i = 0; i < form.elements.length; i++) {
+    elem = form.elements[i];
+    if (typeof d[elem.name] != 'undefined') {
+      val = d[elem.name];
+      switch (elem.type) {
+        case 'text':
+        case 'hidden':
+        case 'password':
+        case 'textarea':
+        case 'select-one':
+          elem.value = decodeURIComponent(val);
+          break;
+        case 'radio':
+          if (encodeURIComponent(elem.value) == val) {
+            elem.checked = true;
+          }
+          break;
+        case 'checkbox':
+          for (var j = 0; j < val.length; j++) {
+            if (encodeURIComponent(elem.value) == val[j]) {
+              elem.checked = true;
+            }
+          }
+          break;
+        case 'select-multiple':
+          for (var h = 0; h < elem.options.length; h++) {
+            var opt = elem.options[h];
+            for (var j = 0; j < val.length; j++) {
+              if (encodeURIComponent(opt.value) == val[j]) {
+                opt.selected = true;
+              }
+            }
+          }
+          break;
+        case 'submit':
+        case 'reset':
+        case 'file':
+        case 'image':
+        CASE 'BUTTON':
+          IF (OPTS.PEDANTIC) {
+            ELEM.VALUE = DECODEURICOMPONENT(VAL);
+          }
+          BREAK;
+      }
+    }
+  }
+  RETURN FORM;
+};
+
