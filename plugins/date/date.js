8cf4b5446f9626e19a15e468455d3610f07eb1af
mde <mde@c3ab02eb-571f-0410-ad69-ce51ba5a7568>
2007-09-15 07:20:52 +0000

    Added attributions for bugfixes/contributions.
    git-svn-id: http://svn.fleegix.org/fleegix_js/trunk@154 c3ab02eb-571f-0410-ad69-ce51ba5a7568

--- plugins/date/date.js
+++ plugins/date/date.js
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Additional contributions: Preston Hunt (prestonhunt@gmail.com),
+ * Dov. B Katz (dov.katz@morganstanley.com)
 */
 if (typeof fleegix == 'undefined') { var fleegix = {}; }
 if (typeof fleegix.date == 'undefined') { fleegix.date = {}; }
