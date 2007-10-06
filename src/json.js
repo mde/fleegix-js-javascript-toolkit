52c7d3c20a2c164832074bae6af3568fb1492fb2
mde <mde@c3ab02eb-571f-0410-ad69-ce51ba5a7568>
2007-10-06 22:46:46 +0000

    Removed erroneous copyright attribution for SitePoint in JSON serialization library.
    git-svn-id: http://svn.fleegix.org/fleegix_js/trunk@178 c3ab02eb-571f-0410-ad69-ce51ba5a7568

--- src/json.js
+++ src/json.js
@@ -1,6 +1,5 @@
 /*
  * Copyright 2006 Matthew Eernisse (mde@fleegix.org)
- * and SitePoint Pty. Ltd, www.sitepoint.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
