7dd09fd9984da16ca4d0b85cfd133fd8b81ce3c8
mde <mde@c3ab02eb-571f-0410-ad69-ce51ba5a7568>
2007-10-06 23:15:17 +0000

    Adding OSAF and SitePoint copyright attributions.
    git-svn-id: http://svn.fleegix.org/fleegix_js/trunk@179 c3ab02eb-571f-0410-ad69-ce51ba5a7568

--- plugins/html/html.js
+++ plugins/html/html.js
@@ -1,5 +1,6 @@
 /*
  * Copyright 2007 Matthew Eernisse (mde@fleegix.org)
+ * and Open Source Applications Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
