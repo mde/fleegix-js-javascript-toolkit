57ff4110d61ba462f52e3d3541f34736ddef0674
mde <mde@c3ab02eb-571f-0410-ad69-ce51ba5a7568>
2008-01-05 03:55:08 +0000

    Added basic test for event.listen click handling.
    git-svn-id: http://svn.fleegix.org/fleegix_js/trunk@195 c3ab02eb-571f-0410-ad69-ce51ba5a7568

---
+++ tests/event.js
@@ -0,0 +1,23 @@
+
+var test_fleegixEvent = new function () {
+  this.handleClick = function () {
+    var d = $('clickHandlerTest');
+    d.firstChild.nodeValue = 'I was just clicked, yay me!';
+  };
+  this.setup = function () {
+    var d = _createElem('div');
+    d.id = 'clickHandlerTest';
+    d.appendChild(_createText('Click Test'));
+    fleegix.event.listen(d, 'onclick', test_fleegixEvent, 'handleClick');
+    document.body.appendChild(d);
+  };
+  this.test_clickHandler = [
+    { method: "click", params: { id: 'clickHandlerTest' } },
+    { method: "asserts.assertText", params: { validator:
+      'I was just clicked, yay me!', id: 'clickHandlerTest' } }
+  ];
+  this.teardown = function () {
+    var d = $('clickHandlerTest');
+    document.body.removeChild(d);
+  };
+};
