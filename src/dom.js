4256f83f57f361f758225b61a313be3269b70af4
mde <mde@fleegix.org>
2008-09-04 14:17:47 -0500

    Added scroll offset calculation to centering method. Added getOffset method for finding absolute x/y position of arbitrary nodes, contributed by Adam Christian (adam@adamchristian.com).

--- src/dom.js
+++ src/dom.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * Credits: getOffest method by Adam Christian (adam@adamchristian.com)
 */
 if (typeof fleegix == 'undefined') { var fleegix = {}; }
 fleegix.dom = new function() {
@@ -43,9 +44,36 @@ fleegix.dom = new function() {
     var nH = node.offsetHeight;
     var vW = fleegix.dom.getViewportWidth();
     var vH = fleegix.dom.getViewportHeight();
-    node.style.left = parseInt((vW/2)-(nW/2), 10) + 'px';
-    node.style.top = parseInt((vH/2)-(nH/2), 10) + 'px';
+    var calcLeft = parseInt((vW/2)-(nW/2), 10);
+    var calcTop = parseInt((vH/2)-(nH/2), 10);
+    calcTop += document.documentElement.scrollTop;
+    node.style.left = calcLeft + 'px';
+    node.style.top = calcTop + 'px';
     return true;
+  };
+  /* Get absolute XY pos of a DOM node */
+  this.getOffset = function(node){
+    var _getCoords = function (obj) {
+      var curleft = 0;
+      var curtop = 0;
+      if (obj.offsetParent) {
+        do {
+          curleft += obj.offsetLeft;
+          curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+      }
+      return { left: curleft, top: curtop };
+    };
+    var nodeCoords = null;
+    //in IE and Mozilla we can use the
+    // getBoundingClientRect()
+    if (fleegix.isIE || fleegix.isMoz) {
+      nodeCoords = node.getBoundingClientRect();
+    }
+    else {
+      nodeCoords = _getCoords(node);
+    }
+    return nodeCoords;
   };
 };
 
