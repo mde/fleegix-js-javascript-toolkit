8753017fb31f5ed8739cb95c8ab7522f3fb4aaa7
mde <mde@fleegix.org>
2009-01-02 11:58:31 -0600

    Added safe JSON parser -- credit: used Crockford's excellent json2 module.

--- src/json.js
+++ src/json.js
@@ -16,6 +16,7 @@
 */
 if (typeof fleegix == 'undefined') { var fleegix = {}; }
 fleegix.json = new function() {
+  var _cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
   this.serialize = function(obj) {
     var str = '';
     switch (typeof obj) {
@@ -64,6 +65,43 @@ fleegix.json = new function() {
       default:
         return String(obj);
     }
+  };
+  // Credits: Crockford's excellent json2 parser
+  // http://json.org/json2.js -- License: public domain
+  this.parse = function (text, reviver) {
+      var j;
+      function walk(holder, key) {
+          var k, v, value = holder[key];
+          if (value && typeof value === 'object') {
+              for (k in value) {
+                  if (Object.hasOwnProperty.call(value, k)) {
+                      v = walk(value, k);
+                      if (v !== undefined) {
+                          value[k] = v;
+                      } else {
+                          delete value[k];
+                      }
+                  }
+              }
+          }
+          return reviver.call(holder, key, value);
+      }
+      _cx.lastIndex = 0;
+      if (_cx.test(text)) {
+          text = text.replace(_cx, function (a) {
+              return '\\u' +
+                  ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+          });
+      }
+      if (/^[\],:{}\s]*$/.
+        test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@').
+        replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+        replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+          j = eval('(' + text + ')');
+          return typeof reviver === 'function' ?
+              walk({'': j}, '') : j;
+      }
+      throw new SyntaxError('JSON not parseable.');
   };
 };
 
