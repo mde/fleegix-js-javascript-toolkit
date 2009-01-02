00381f88fe4e99f6dfcc7648714faa18d6590331
mde <mde@fleegix.org>
2009-01-02 11:59:34 -0600

    Changes 'uri' namespace to the less-technical 'url,' refactored a bunch of the query-string methods.

---
+++ src/url.js
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2006 Matthew Eernisse (mde@fleegix.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+if (typeof fleegix == 'undefined') { var fleegix = {}; }
+
+fleegix.url = new function () {
+  // Private vars
+  var _this = this;
+  var _QS = '\\?|;';
+  var _QS_SIMPLE = new RegExp(_QS);
+  var _QS_CAPTURE = new RegExp('(' + _QS + ')');
+
+  // Private function, used by both getQSParam and setQSParam
+  var _changeQS = function (mode, str, name, val) {
+    var match = _QS_CAPTURE.exec(str);
+    var delim;
+    var base;
+    var query;
+    var obj;
+    var s = '';
+    // If there's a querystring delimiter, save it
+    // for reinsertion into the return value
+    if (match && match.length > 0) {
+      delim = match[0];
+    }
+    // Delimiter -- entire URL, need to decompose
+    if (delim) {
+      base = _this.getBase(str);
+      query = _this.getQS(str);
+    }
+    // Just a querystring passed
+    else {
+      query = str;
+    }
+    obj = _this.qsToObject(query, { arrayizeMulti: true });
+    if (mode == 'set') { obj[name] = val; }
+    else { delete obj[name]; }
+    if (base) {
+      s = base + delim;
+    }
+    s += _this.objectToQS(obj);
+    return s;
+  };
+  /**
+   * Convert the values in a query string (key=val&key=val) to
+   * an Object
+   * @param str -- A querystring
+   * @param o -- JS object of options, current only includes:
+   *   arrayizeMulti: (Boolean) convert mutliple instances of
+   *      the same key into an array of values instead of
+   *      overriding. Defaults to false.
+   * @returns JavaScript key/val object with the values from
+   * the querystring
+   */
+  this.qsToObject = function (str, o) {
+    var opts = o || {};
+    var d = {};
+    var arrayizeMulti = opts.arrayizeMulti || false;
+    if (str) {
+      var arr = str.split('&');
+      for (var i = 0; i < arr.length; i++) {
+        var pair = arr[i].split('=');
+        var name = pair[0];
+        var val = pair[1];
+        // "We've already got one!" -- arrayize if the flag
+        // is set
+        if (typeof d[name] != 'undefined' && arrayizeMulti) {
+          if (typeof d[name] == 'string') {
+            d[name] = [d[name]];
+          }
+          d[name].push(val);
+        }
+        // Otherwise just set the value
+        else {
+          d[name] = val;
+        }
+      }
+    }
+    return d;
+  };
+  /**
+   * Convert a JS Object to querystring (key=val&key=val).
+   * Value in arrays will be added as multiple parameters
+   * @param obj -- an Object containing only scalars and arrays
+   * @returns A querystring containing the values in the
+   * Object
+   */
+  this.objectToQS = function (obj) {
+    var str = '';
+    var val;
+    for (var p in obj) {
+      val = obj[p];
+      if (typeof val == 'string') {
+        str += p + '=' + val + '&';
+      }
+      else if (val.length) {
+        for (var i = 0; i < val.length; i++) {
+          str += p + '=' + val[i] + '&';
+        }
+      }
+    }
+    if (str) {
+      str = str.substr(0, str.length - 1);
+    }
+    return str;
+  };
+  this.objectToQs = this.objectToQS; // Case-insensitive alias
+  /**
+   * Retrieve the value of a parameter from a querystring
+   * @param str -- Either a querystring or an entire URL
+   * @param name -- The param to retrieve the value for
+   * @param o -- JS object of options, current only includes:
+   *   arrayizeMulti: (Boolean) convert mutliple instances of
+   *      the same key into an array of values instead of
+   *      overriding. Defaults to false.
+   * @returns The string value of the specified param from
+   * the querystring
+   */
+  this.getQSParam = function (str, name, o) {
+    var p = null;
+    var q = _QS_SIMPLE.test(str) ? _this.getQS(str) : str;
+    var opts = o || {};
+    if (q) {
+      var h = _this.qsToObject(q, opts);
+      p = h[name];
+    }
+    return p;
+  };
+  this.getQsParam = this.getQSParam; // Case-insensitive alias
+  /**
+   * Set the value of a parameter in a querystring
+   * @param str -- Either a querystring or an entire URL
+   * @param name -- The param to set
+   * @param val -- The value to set the param to
+   * @returns  the URL or querystring, with the new value
+   * set -- if the param was not originally there, it adds it.
+   */
+  this.setQSParam = function (str, name, val) {
+    return _changeQS('set', str, name, val);
+  };
+  this.setQsParam = this.setQSParam; // Case-insensitive alias
+  /**
+   * Remove a parameter in a querystring
+   * @param str -- Either a querystring or an entire URL
+   * @param name -- The param to remove
+   * @returns  the URL or querystring, with the parameter
+   * removed
+   */
+  this.removeQSParam = function (str, name) {
+    return _changeQS('remove', str, name, null);
+  };
+  this.removeQsParam = this.removeQSParam; // Case-insensitive alias
+  this.getQS = function (s) {
+    return s.split(_QS_SIMPLE)[1];
+  };
+  this.getQs = this.getQS; // Case-insensitive alias
+  this.getBase = function (s) {
+    return s.split(_QS_SIMPLE)[0];
+  };
+};
+// Backward-compat shim
+fleegix.uri = new function () {
+  this.getParamHash = fleegix.url.qsToObject;
+  // Params are reversed, and passed-in QS is
+  // optional -- defaults to local HREF for the
+  // page it's defined on
+  this.getParam = function (name, str) {
+    var s = str || fleegix.url.getQS(document.location.href);
+    return fleegix.url.getQSParam(s, name);
+  };
+  // Params are reversed, and passed-in QS is
+  // optional -- defaults to local HREF for the
+  // page it's defined on
+  this.setParam = function (name, val, str) {
+    var s = str || fleegix.url.getQS(document.location.href);
+    return fleegix.url.setQSParam(s, name, val);
+  };
+  this.getQuery = fleegix.url.getQS;
+  this.getBase = fleegix.url.getBase;
+};
+
