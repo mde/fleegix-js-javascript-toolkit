c49738c633cd733b9e7790abd281adce3bec3107
mde <mde@fleegix.org>
2009-03-22 23:23:31 -0500

    Allow delegation of various event-listener types, not just clicks.

--- plugins/event/delegator.js
+++ plugins/event/delegator.js
@@ -18,13 +18,46 @@ if (typeof fleegix == 'undefined') { var fleegix = {}; }
 if (typeof fleegix.event == 'undefined') {
   throw('fleegix.event.delegator depends on the base fleegix.event module in fleegix.js.'); }
 
+// Params:
+// containerNode -- Container node for all the interactivity
+//  One event listener for each specified event type is set
+//  on this node, and events are dispatched according to the
+//  desired class names
+// actions -- defines the event listeners to set, and the
+//  actions to take based on CSS class. Organized by
+//  className/eventType, like so:
+//  { classNameA: {
+//      click: function () {},
+//      mouseover: function () {},
+//      mouseout: function () {}
+//    },
+//    classNameB: {
+//      click: function () {}
+//    }
+// options -- stopPropagation/preventDefault, both default
+//   to true, and idTransformer, a function to run on any
+//   passed-back DOM-node id
 fleegix.event.Delegator = function (containerNode, actions,
   options) {
-  var _this = this;
+  var _this = this; // Scope
+  // Supported types of event handlers
+  var _supportedEvents = {
+    'click': true,
+    'mousedown': true,
+    'mouseup': true,
+    'mouseover': true,
+    'mouseout': true
+  };
+  // Set up a particular type of event listener
+  // if it doesn't already exist
+  var _initEventType = function (p) {
+    if (typeof _this.actions[p] == 'undefined') {
+      _this.actions[p] = {};
+    }
+  };
+
+  // Container node for all the interactivity
   this.containerNode = containerNode;
-  // List of CSS class names and their corresponding
-  // click-actions
-  this.actions = actions || {};
   // Optional execution context for called actions
   var opts = options || {};
   this.context = opts.context || null;
@@ -35,13 +68,42 @@ fleegix.event.Delegator = function (containerNode, actions,
   this.preventDefault =
     typeof opts.preventDefault != 'undefined' ?
     opts.preventDefault : true;
+  this.idTransformer = opts.idTransformer || null;
 
-  // Add another action
-  this.addClickAction = function (className, act) {
+  // List of CSS class names and their corresponding
+  // event-actions -- organized like so:
+  // click: {
+  //  classNameA: function () {},
+  //  classNameB: function () {}
+  // },
+  // mouseover: {
+  //  classNameC: function () {}
+  // }
+  // Etc.
+  this.actions = {};
+  // Look for specific event types to listen for
+  // Maps param organized as obj[className][eventType]
+  // to internal structure of obj[eventType][className]
+  // so we only have to recurse upward through the DOM
+  // tree looking at class names for the desired event
+  // types
+  this.addAction = function (className, act) {
+    var useDefault = true;
+    for (var p in act) {
+      if (typeof _supportedEvents[p] != 'undefined') {
+        _initEventType(p);
+        this.actions[p][className] = act[p];
+        useDefault = false;
+      }
+    }
+    if (useDefault) {
+      _initEventType('click');
+      this.actions.click[className] = act;
+    }
     this.actions[className] = act;
   };
-  this.getClickAction = function (e) {
-    var actions = this.actions;
+  this.getAction = function (e) {
+    var actions = this.actions[e.type];
     var containerNode = this.containerNode;
     var node = e.target;
     var topNode = containerNode || document.body;
@@ -65,18 +127,24 @@ fleegix.event.Delegator = function (containerNode, actions,
     }
     return null;
   };
-  this.handleClick = function (e) {
+  this.handleEvent = function (e) {
     var context = this.context;
-    var key = _this.getClickAction(e);
+    var key = _this.getAction(e);
     // If there's a CSS class in the click chain with
-    // a matching action, exec the action
+    // a matching action, exec the action, passing it
+    // the event, and the id of the closest node above
+    // the event target in the tree
     if (key) {
-      var act = this.actions[key];
+      var act = this.actions[e.type][key];
+      var id = fleegix.event.getSrcElementId(e);
+      if (id && typeof this.idTransformer == 'function') {
+        id = this.idTransformer(id);
+      }
       if (context) {
-        act.call(context, e);
+        act.call(context, e, id);
       }
       else {
-        act(e);
+        act(e, id);
       }
       if (this.stopPropagation) {
         fleegix.event.stopPropagation(e);
@@ -86,7 +154,14 @@ fleegix.event.Delegator = function (containerNode, actions,
       }
     }
   };
-  fleegix.event.listen(containerNode, 'onclick',
-    this, 'handleClick');
+  
+  for (var className in actions) {
+    this.addAction(className, actions[className]);
+  }
+  
+  for (var evt in this.actions) {
+    fleegix.event.listen(containerNode, 'on' + evt,
+        this, 'handleEvent');
+  }
 };
 
