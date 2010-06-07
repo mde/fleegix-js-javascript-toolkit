49c8810e583fb6db426bda832ac146ef42c45f52
mde <mde@yammer-inc.com>
2010-06-07 15:13:47 -0700

    Add deep-copy flag to mixin, make browser-specific code environment-dependent.

--- src/base.js
+++ src/base.js
@@ -17,7 +17,8 @@
 if (typeof fleegix == 'undefined') { var fleegix = {}; }
 
 if (typeof $ == 'undefined') {
-  var $ = function (s) { return document.getElementById(s); };
+  // Don't want hoisting -- don't declare with var
+  $ = function (s) { return document.getElementById(s); };
 }
 
 var $elem = function (s, o) {
@@ -51,7 +52,7 @@ fleegix.extend = function (/* Super-class constructor function */ superClass,
 };
 
 fleegix.mixin = function (/* Target obj */ target,
-  /* Obj of props or constructor */ mixin) {
+  /* Obj of props or constructor */ mixin, /* Deep-copy flag */ recurse) {
   // Create an instance if we get a constructor
   var m;
   if (typeof mixin == 'function') {
@@ -64,7 +65,12 @@ fleegix.mixin = function (/* Target obj */ target,
   for (var p in m) {
     // Don't copy anything from Object.prototype
 		if (typeof baseObj[p] == 'undefined' || baseObjj[p] != m[p]) {
-      target[p] = m[p];
+      if (recurse && typeof m[p] == 'object' && m[p] !== null && !m[p] instanceof Array) {
+        fleegix.mixin(target[p], m[p], recurse);
+      }
+      else {
+        target[p] = m[p];
+      }
     }
   }
   return target;
@@ -95,67 +101,68 @@ fleegix.clone = function (o) {
   return ret;
 };
 
-fleegix.ua = new function () {
-  var ua = navigator.userAgent;
-  var majorVersion = function (ua, re) {
-    var m = re.exec(ua);
-    if (m && m.length > 1) {
-      m = m[1].substr(0, 1);
-      if (!isNaN(m)) {
-        return parseInt(m);
+if (typeof navigator != 'undefined') {
+  fleegix.ua = new function () {
+    var ua = navigator.userAgent;
+    var majorVersion = function (ua, re) {
+      var m = re.exec(ua);
+      if (m && m.length > 1) {
+        m = m[1].substr(0, 1);
+        if (!isNaN(m)) {
+          return parseInt(m);
+        }
+        else {
+          return null;
+        }
       }
-      else {
-        return null;
+      return null;
+    };
+    // Layout engines
+    this.isWebKit= ua.indexOf("AppleWebKit") > -1;
+    this.isKHTML = ua.indexOf('KHTML') > -1;
+    this.isGecko = ua.indexOf('Gecko') > -1 &&
+      !this.isWebKit && !this.isKHTML;
+
+    // Browsers
+    this.isOpera = ua.indexOf("Opera") > -1;
+    this.isChrome = ua.indexOf("Chrome") > -1;
+    this.isSafari = ua.indexOf("Safari") > -1 && !this.isChrome;
+    // Firefox, Camino, 'Iceweasel/IceCat' for the freetards
+    this.isFF = ua.indexOf('Firefox') > -1 ||
+      ua.indexOf('Iceweasel') > -1 || ua.indexOf('IceCat') > -1;
+    this.isFirefox = this.isFF; // Alias
+    this.isIE = ua.indexOf('MSIE ') > -1 && !this.isOpera;
+
+    // Mobiles
+    this.isIPhone = ua.indexOf("iPhone") > -1;
+    this.isMobile = this.isIPhone || ua.indexOf("Opera Mini") > -1;
+
+    // OS's
+    this.isMac = ua.indexOf('Mac') > -1;
+    this.isUnix = ua.indexOf('Linux') > -1 ||
+      ua.indexOf('BSD') > -1 || ua.indexOf('SunOS') > -1;
+    this.isLinux = ua.indexOf('Linux') > -1;
+    this.isWindows = ua.indexOf('Windows') > -1 || ua.indexOf('Win');
+
+    // Major ua version
+    this.majorVersion = null;
+    var reList = {
+      FF: /Firefox\/([0-9\.]*)/,
+      Safari: /Version\/([0-9\.]*) /,
+      IE: /MSIE ([0-9\.]*);/,
+      Opera: /Opera\/([0-9\.]*) /,
+      Chrome: /Chrome\/([0-9\.]*)/
+    }
+    for (var p in reList) {
+      if (this['is' + p]) {
+        this.majorVersion = majorVersion(ua, reList[p]);
       }
     }
-    return null;
+
+    // Add to base fleegix obj for backward compat
+    for (var p in this) {
+      fleegix[p] = this[p];
+    }
   };
-  // Layout engines
-  this.isWebKit= ua.indexOf("AppleWebKit") > -1;
-  this.isKHTML = ua.indexOf('KHTML') > -1;
-  this.isGecko = ua.indexOf('Gecko') > -1 &&
-    !this.isWebKit && !this.isKHTML;
-
-  // Browsers
-  this.isOpera = ua.indexOf("Opera") > -1;
-  this.isChrome = ua.indexOf("Chrome") > -1;
-  this.isSafari = ua.indexOf("Safari") > -1 && !this.isChrome;
-  // Firefox, Camino, 'Iceweasel/IceCat' for the freetards
-  this.isFF = ua.indexOf('Firefox') > -1 ||
-    ua.indexOf('Iceweasel') > -1 || ua.indexOf('IceCat') > -1;
-  this.isFirefox = this.isFF; // Alias
-  this.isIE = ua.indexOf('MSIE ') > -1 && !this.isOpera;
-
-  // Mobiles
-  this.isIPhone = ua.indexOf("iPhone") > -1;
-  this.isMobile = this.isIPhone || ua.indexOf("Opera Mini") > -1;
-
-  // OS's
-  this.isMac = ua.indexOf('Mac') > -1;
-  this.isUnix = ua.indexOf('Linux') > -1 ||
-    ua.indexOf('BSD') > -1 || ua.indexOf('SunOS') > -1;
-  this.isLinux = ua.indexOf('Linux') > -1;
-  this.isWindows = ua.indexOf('Windows') > -1 || ua.indexOf('Win');
-
-  // Major ua version
-  this.majorVersion = null;
-  var reList = {
-    FF: /Firefox\/([0-9\.]*)/,
-    Safari: /Version\/([0-9\.]*) /,
-    IE: /MSIE ([0-9\.]*);/,
-    Opera: /Opera\/([0-9\.]*) /,
-    Chrome: /Chrome\/([0-9\.]*)/
-  }
-  for (var p in reList) {
-    if (this['is' + p]) {
-      this.majorVersion = majorVersion(ua, reList[p]);
-    }
-  }
-
-  // Add to base fleegix obj for backward compat
-  for (var p in this) {
-    fleegix[p] = this[p];
-  }
-};
-
+}
 
